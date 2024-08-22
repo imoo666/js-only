247c955de9ca49f824ae04134238e20a6a5cd9be
imoo <859416516@qq.com>
2024-08-22 20:34:39 +0800

    fix: 修复单引号问题

--- readAndEscape.js
+++ readAndEscape.js
@@ -1,12 +1,16 @@
 import fs from "fs";
 
 // 读取压缩后的 HTML 文件
-fs.readFile("./dist/index-zip.html", "utf8", (_, data) => {
+fs.readFile("./dist/output.html", "utf8", (err, data) => {
+  if (err) throw err;
+
   // 将 HTML 转换为 C 字符串格式
   const cString = `String htmlString = "${data
-    .replace(/"/g, '\\"')
+    .replace(/\\/g, "\\\\") // 转义反斜杠
+    .replace(/"/g, '\\"') // 转义双引号
+    .replace(/'/g, "\\'") // 转义单引号
     .replace(/\n/g, "\\n")}";\n`;
 
   // 将结果写入 C 文件
-  fs.writeFileSync("./dist/index-zip.c", cString);
+  fs.writeFileSync("./dist/output.c", cString);
 });
